7a2f83cde57b87851ed8c9337475af31b48be7a4
Semyon Katz <kavabunga@gmail.com>
2024-03-11 17:17:36 +0200

    refactor: :recycle: fix navigation component js, add bootstrap grid, adopt for mobile
    - Починил проблему с методом (именно по этой причине в браузере переставал работать компонент навигации) — теперь компонент использует хэш-ссылки и атрибут . Все работает, кольца крутятся, ссылки работают. Кстати, стало возможно по ссылке из строки браузера перейти сразу в нужный раздел   - Добавил стилизацию сетки с использованием Bootstrap 5   - Настроил базовую адаптивность. Для отображения меню на маленьких экранах добавил кнопку-гамбургер по которой меню открывается в верхней части экрана

--- js/main.js
+++ js/main.js
@@ -1,128 +1,117 @@
-jQuery(document).ready(function($){
-	var articlesWrapper = $('.cd-articles');
+jQuery(document).ready(function ($) {
+  var articlesWrapper = $('.cd-articles');
 
-	if( articlesWrapper.length > 0 ) {
-		// cache jQuery objects
-		var windowHeight = $(window).height(),
-			articles = articlesWrapper.find('article'),
-			aside = $('.cd-read-more'),
-			articleSidebarLinks = aside.find('li');
-		// initialize variables
-		var	scrolling = false,
-			sidebarAnimation = false,
-			resizing = false,
-			mq = checkMQ(),
-			svgCircleLength = parseInt(Math.PI*(articleSidebarLinks.eq(0).find('circle').attr('r')*2));
-		
-		// check media query and bind corresponding events
-		if( mq == 'desktop' ) {
-			$(window).on('scroll', checkRead);
-			$(window).on('scroll', checkSidebar);
-		}
+  if (articlesWrapper.length > 0) {
+    // cache jQuery objects
+    var windowHeight = $(window).height(),
+      articles = articlesWrapper.find('article'),
+      aside = $('.cd-read-more'),
+      articleSidebarLinks = aside.find('li');
+    // initialize variables
+    var scrolling = false,
+      mq = checkMQ(),
+      svgCircleLength = parseInt(
+        Math.PI * (articleSidebarLinks.eq(0).find('circle').attr('r') * 2),
+      );
 
-		$(window).on('resize', resetScroll);
+    // check media query and bind corresponding events
+    if (mq == 'desktop') {
+      $(window).on('resize', checkMenuState);
+    }
+    $(window).on('scroll', checkRead);
 
-		updateArticle();
-		updateSidebarPosition();
+    updateArticle();
 
-		aside.on('click', 'a', function(event){
-			event.preventDefault();
-			var selectedArticle = articles.eq($(this).parent('li').index()),
-				selectedArticleTop = selectedArticle.offset().top;
+    aside.on('click', 'a', function (event) {
+      event.preventDefault();
+      var selectedArticle = articles.eq($(this).parent('li').index()),
+        selectedArticleTop = selectedArticle.offset().top;
 
-			$(window).off('scroll', checkRead);
+      $(window).off('scroll', checkRead);
 
-			$('body,html').animate(
-				{'scrollTop': selectedArticleTop + 2}, 
-				300, function(){
-					checkRead();
-					$(window).on('scroll', checkRead);
-				}
-			); 
-	    });
-	}
+      $('body,html').animate(
+        { scrollTop: selectedArticleTop + 2 },
+        300,
+        function () {
+          checkRead();
+          $(window).on('scroll', checkRead);
+        },
+      );
+    });
+  }
 
-	function checkRead() {
-		if( !scrolling ) {
-			scrolling = true;
-			(!window.requestAnimationFrame) ? setTimeout(updateArticle, 300) : window.requestAnimationFrame(updateArticle);
-		}
-	}
+  function checkRead() {
+    if (!scrolling) {
+      scrolling = true;
+      !window.requestAnimationFrame
+        ? setTimeout(updateArticle, 300)
+        : window.requestAnimationFrame(updateArticle);
+    }
+  }
 
-	function checkSidebar() {
-		if( !sidebarAnimation ) {
-			sidebarAnimation = true;
-			(!window.requestAnimationFrame) ? setTimeout(updateSidebarPosition, 300) : window.requestAnimationFrame(updateSidebarPosition);
-		}
-	}
+  function updateArticle() {
+    var scrollTop = $(window).scrollTop();
 
-	function resetScroll() {
-		if( !resizing ) {
-			resizing = true;
-			(!window.requestAnimationFrame) ? setTimeout(updateParams, 300) : window.requestAnimationFrame(updateParams);
-		}
-	}
+    articles.each(function () {
+      var article = $(this),
+        articleTop = article.offset().top,
+        articleHeight = article.outerHeight(),
+        articleSidebarLink = articleSidebarLinks
+          .eq(article.index())
+          .children('a');
 
-	function updateParams() {
-		windowHeight = $(window).height();
-		mq = checkMQ();
-		$(window).off('scroll', checkRead);
-		$(window).off('scroll', checkSidebar);
-		
-		if( mq == 'desktop') {
-			$(window).on('scroll', checkRead);
-			$(window).on('scroll', checkSidebar);
-		}
-		resizing = false;
-	}
+      if (article.is(':last-of-type'))
+        articleHeight = articleHeight - windowHeight;
 
-	function updateArticle() {
-		var scrollTop = $(window).scrollTop();
+      if (articleTop > scrollTop) {
+        articleSidebarLink.removeClass('read reading');
+      } else if (
+        scrollTop >= articleTop &&
+        articleTop + articleHeight > scrollTop
+      ) {
+        var dashoffsetValue =
+          svgCircleLength * (1 - (scrollTop - articleTop) / articleHeight);
+        articleSidebarLink
+          .addClass('reading')
+          .removeClass('read')
+          .find('circle')
+          .attr({ 'stroke-dashoffset': dashoffsetValue });
+        changeHash(articleSidebarLink.attr('href'));
+      } else {
+        articleSidebarLink.removeClass('reading').addClass('read');
+      }
+    });
+    scrolling = false;
+  }
 
-		articles.each(function(){
-			var article = $(this),
-				articleTop = article.offset().top,
-				articleHeight = article.outerHeight(),
-				articleSidebarLink = articleSidebarLinks.eq(article.index()).children('a');
+  function changeHash(link) {
+    var currentPage = location.hash;
+    if (link != currentPage && history.pushState)
+      window.history.pushState({ hash: link }, '', link);
+  }
 
-			if( article.is(':last-of-type') ) articleHeight = articleHeight - windowHeight;
+  function checkMQ() {
+    return window
+      .getComputedStyle(articlesWrapper.get(0), '::before')
+      .getPropertyValue('content')
+      .replace(/'/g, '')
+      .replace(/"/g, '');
+  }
 
-			if( articleTop > scrollTop) {
-				articleSidebarLink.removeClass('read reading');
-			} else if( scrollTop >= articleTop && articleTop + articleHeight > scrollTop) {
-				var dashoffsetValue = svgCircleLength*( 1 - (scrollTop - articleTop)/articleHeight);
-				articleSidebarLink.addClass('reading').removeClass('read').find('circle').attr({ 'stroke-dashoffset': dashoffsetValue });
-				changeUrl(articleSidebarLink.attr('href'));
-			} else {
-				articleSidebarLink.removeClass('reading').addClass('read');
-			}
-		});
-		scrolling = false;
-	}
+  $('.menu-show-button').click(function () {
+    $('.cd-read-more-container').show();
+  });
 
-	function updateSidebarPosition() {
-		var articlesWrapperTop = articlesWrapper.offset().top,
-			articlesWrapperHeight = articlesWrapper.outerHeight(),
-			scrollTop = $(window).scrollTop();
+  $('.menu-hide-button').click(function () {
+    $('.cd-read-more-container').hide();
+  });
 
-		if( scrollTop < articlesWrapperTop) {
-			aside.removeClass('fixed').attr('style', '');
-		} else if( scrollTop >= articlesWrapperTop && scrollTop < articlesWrapperTop + articlesWrapperHeight - windowHeight) {
-			aside.addClass('fixed').attr('style', '');
-		} else {
-			var articlePaddingTop = Number(articles.eq(1).css('padding-top').replace('px', ''));
-			if( aside.hasClass('fixed') ) aside.removeClass('fixed').css('top', articlesWrapperHeight + articlePaddingTop - windowHeight + 'px');
-		}
-		sidebarAnimation =  false;
-	}
-
-	function changeUrl(link) {
-		var pageArray = location.pathname.split('/'),
-        	actualPage = pageArray[pageArray.length - 1] ;
-        if( actualPage != link && history.pushState ) window.history.pushState({path: link},'',link);
-	}
-
-	function checkMQ() {
-		return window.getComputedStyle(articlesWrapper.get(0), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "");
-	}
+  function checkMenuState() {
+    if ($(window).width() >= '993') {
+      $('.cd-read-more-container').show();
+    }
+    if ($(window).width() < '993') {
+      $('.cd-read-more-container').hide();
+    }
+  }
 });
